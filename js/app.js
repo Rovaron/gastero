4a1504bae4898ca010751d021fe67ca246cd92ac
Guilherme Rovaron <Guilherme Rovaron>
2016-08-30 17:51:12 -0300

    gastos mvp

--- js/app.js
+++ js/app.js
@@ -1,16 +1,39 @@
-Vue.use(Keen);
-
 new Vue({
     el: 'body',
-    components: {
-
-    },
     data: {
         msg: "hello world",
         gastos: [
-            {name: "Cigarro", valor: "7.75"},
-            {name: "Cerveja", valor: "7,00"},
-            {name: "Pastel", valor: "4,00"}
-        ]
+            {nome: "Cigarro", valor: 7.75},
+            {nome: "Cerveja", valor: 7.00},
+            {nome: "Pastel", valor: 4.00}
+        ],
+        gasto: {nome: '', valor: ''}
+    },
+    methods: {
+        novoGasto: function(e){
+            e.preventDefault();
+            this.gastos.push(this.gasto);
+            this.gasto = {};
+        },
+        removeGasto: function(gasto){
+            this.gastos.$remove(gasto);
+        },
+        clearZero: function(){
+            if(this.gasto.valor == 0){
+                this.gasto.valor = "";
+            }
+        }
+    },
+    computed: {
+        isValid: function() {
+            return this.gasto.nome != '' && this.gasto.valor != '' && typeof this.gasto.valor == "number";
+        },
+        totalGastos: function(){
+            var sum = 0;
+            this.gastos.reduce(function(prev, gasto){
+               sum += gasto.valor;
+            },0);
+            return sum;
+        }
     }
 });
